ed7ed056ccbc9597ffa7b87dd1849ff63280e507
si3mshady <cloudwarrior2891@gmail.com>
2021-04-20 08:39:50 -0500

    deployed with hardhat

--- contracts/FirstNFT.sol
+++ contracts/FirstNFT.sol
@@ -1,21 +1,22 @@
- //Contractbasedonhttps://docs.openzeppelin.com/contracts/3.x/erc721
- //SPDX-License-Identifier:MIT
- pragma solidity ^0.7.3;
+//Contractbasedonhttps://docs.openzeppelin.com/contracts/3.x/erc721
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.7.3;
 
- import"@openzeppelin/contracts/token/ERC721/ERC721.sol";
- import"@openzeppelin/contracts/utils/Counters.sol";
- import"@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
- contract MyNFTisERC721 ,Ownable {
- using Counters for Counters.Counter;
- Counters.Counter private _tokenIds;
+contract MyNFT is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
- constructor() public ERC721("si3mshady", "si3mshadyNFT") {} 
- 
-        function mintNFT(address recipient, string memory tokenURI)
-        public onlyOwner
+    constructor() public ERC721("elNFT", "shadyEl") {}
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        onlyOwner
         returns (uint256)
-        {
+    {
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
@@ -23,5 +24,5 @@
         _setTokenURI(newItemId, tokenURI);
 
         return newItemId;
- }
- }
+    }
+}
